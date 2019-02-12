466c0942b6c735125a6945d86772bbde67c47b93
kenjirai <kenjirai@tuta.io>
2019-02-12 10:53:15 +0545

    Updates

---
+++ precache-manifest.a9e727df4c21e370410e7be672cc7de8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d4c41dabf015e7558a",
+    "url": "/sign-the-doc/static/js/runtime~main.d9d4c41d.js"
+  },
+  {
+    "revision": "35c664f1369f4da489e6",
+    "url": "/sign-the-doc/static/js/main.35c664f1.chunk.js"
+  },
+  {
+    "revision": "f701fda91faf4ebd4b76",
+    "url": "/sign-the-doc/static/js/2.f701fda9.chunk.js"
+  },
+  {
+    "revision": "35c664f1369f4da489e6",
+    "url": "/sign-the-doc/static/css/main.66f29e42.chunk.css"
+  },
+  {
+    "revision": "1e72f604efe2e68b3252069b4a4982ce",
+    "url": "/sign-the-doc/index.html"
+  }
+];
